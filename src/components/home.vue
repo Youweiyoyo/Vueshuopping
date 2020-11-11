48d825e6abaab7610409e524736a83f8c945170b
Youwei_star <1176071733@qq.com>
2020-11-11 21:30:27 +0800

    主页开始开发

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <el-button type="info" @click="loginout">退出</el-button>
-  </div>
+  <el-container class="homeContainer">
+    <!-- 头部区域 -->
+    <el-header>
+      Header
+      <el-button type="info" @click="loginout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -15,4 +26,18 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.el-header{
+  background-color: #373d41;
+}
+.el-aside{
+  background-color: #333744;
+}
+.el-main{
+  background-color: #eaedf1;
+}
+.homeContainer{
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,11 +4,19 @@ import {
   FromItem,
   Input,
   Button,
-  Message
+  Message,
+  header,
+  container,
+  main,
+  aside
 } from 'element-ui'
 // 导入弹出框组件
 Vue.use(Button)
 Vue.use(From)
 Vue.use(FromItem)
 Vue.use(Input)
+Vue.use(header)
+Vue.use(container)
+Vue.use(main)
+Vue.use(aside)
 Vue.prototype.$message = Message

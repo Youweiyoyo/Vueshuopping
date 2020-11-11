e1f7d9b6e0cb5ccf1ff74ed40fa54d5c99475972
Youwei_star <1176071733@qq.com>
2020-11-11 17:02:06 +0800

    logindemglu

--- src/components/login.vue
+++ src/components/login.vue
@@ -6,9 +6,32 @@
         <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 表单区域 -->
-      <el-form label-width="80px">
-        <el-form-item label="活动名称">
-          <el-input></el-input>
+      <el-form
+        label-width="0px"
+        class="login_form"
+        :model="loginFrom"
+        :rules="loginFromRules"
+        ref="loginFromRef"
+      >
+        <!-- 用户名框 -->
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="iconfont icon-user"
+            v-model="loginFrom.username"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码框-->
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="iconfont icon-lock_fill"
+            v-model="loginFrom.password"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" round @click="yzFrom">登录</el-button>
+          <el-button type="info" round @click="restLoginFrom">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -16,7 +39,52 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // 登录表单的数据绑定对象
+      loginFrom: {
+        username: "",
+        password: "",
+      },
+      loginFromRules: {
+        username: [
+          { required: true, message: "请输入用户名称", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请输入用户名称", trigger: "blur" },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    // 点击重置按钮，重置登录表单
+    restLoginFrom() {
+      // console.log(this);
+      this.$refs.loginFromRef.resetFields();
+    },
+    yzFrom() {
+      this.$$refs.loginFromRef.validate(async valid => {
+        if (!valid) return;
+        // 发起请求
+        const result = await this.$http.post("login", this.loginFrom)
+        console.log(result)
+      });
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -51,6 +119,17 @@ export default {};
       background-color: #eeee;
     }
   }
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
 </style>
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,6 @@
-import ElementUI, { Form } from 'element-ui'
 import Vue from 'vue'
 import { Button } from 'element-ui'
-import { From, FromItem } from 'element-ui'
-import { Input } from 'element-ui'
+import { From, FromItem, Input } from 'element-ui'
 Vue.use(Button)
 Vue.use(From)
 Vue.use(FromItem)
